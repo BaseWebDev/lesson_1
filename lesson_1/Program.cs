685cd510fa71f2f5825fc32c8c8011d144946603
SermusAnton <antonsermus@mail.ru>
2018-04-05 12:54:37 +0300

    Delete extra list and rename variable

--- lesson_1/Program.cs
+++ lesson_1/Program.cs
@@ -12,19 +12,19 @@ namespace lesson_1 {
             // Используя Random и Sort
             List<Name> Names = new List<Name>();  
             Random randomN = new Random();           
-            Names.Add(new Name(randomN.Next(0,100),"Вахид"));
-            Names.Add(new Name(randomN.Next(0,100),"Антон"));
-            Names.Add(new Name(randomN.Next(0,100),"Николай"));
-            Names.Add(new Name(randomN.Next(0,100),"Антон"));
-            Names.Add(new Name(randomN.Next(0,100),"Паша"));
-            Names.Add(new Name(randomN.Next(0,100),"Евгений"));
+            Names.Add(new Name("Вахид"));
+            Names.Add(new Name("Антон"));
+            Names.Add(new Name("Николай"));
+            Names.Add(new Name("Антон"));
+            Names.Add(new Name("Паша"));
+            Names.Add(new Name("Евгений"));
             Console.WriteLine("Последовательно: ");
             foreach (Name key in Names) {
                 Console.WriteLine("Случайное число: "+ key.RandomNomer + " имя ученика: " + key.Disciple);
             }
             Console.WriteLine();
             Names.Sort();  // Сортируем, точнее перемешиваем
-            Console.WriteLine("Случайно: ");
+            Console.WriteLine("Случайно используя GetHash: ");
             foreach (Name key in Names) {
                 Console.WriteLine("Случайное число: " + key.RandomNomer + " имя ученика: " + key.Disciple);
             }
@@ -39,44 +39,30 @@ namespace lesson_1 {
                 Console.WriteLine("Случайное число: " + key.RandomNomer + " имя ученика: " + key.Disciple);
             }
             Console.WriteLine();
-            // Используя GetHash для однократного перемешивания
-            List<Name> NamesUseHash = new List<Name>();
-            NamesUseHash.Add(new Name( "Вахид"));
-            NamesUseHash.Add(new Name("Антон"));
-            NamesUseHash.Add(new Name("Николай"));
-            NamesUseHash.Add(new Name("Антон"));
-            NamesUseHash.Add(new Name("Паша"));
-            NamesUseHash.Add(new Name("Евгений"));
-            NamesUseHash.Sort();
-            Console.WriteLine("Используя GetHash и Sort: ");
-            foreach (Name key in NamesUseHash) {
-                Console.WriteLine("\t " + key.Disciple);
-            }
-            Console.WriteLine();
             // Перемешать используя алгоритм Фишера-Йетса
-            Shuffle(NamesUseHash);
+            Shuffle(Names);
             Console.WriteLine("Используя Алгоритм Фишера – Йетса: ");
-            foreach (Name key in NamesUseHash) {
+            foreach (Name key in Names) {
                 Console.WriteLine("\t " + key.Disciple);
             }
             Console.WriteLine();           
             Console.ReadKey();
         }
 
-        private static Random rng = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
 
+        private static Random randomChange = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
         /// <summary>
         /// Алгоритм Фишера – Йетса (Современная версия)
         /// http://vscode.ru/prog-lessons/kak-peremeshat-massiv-ili-spisok.html
         /// https://ru.stackoverflow.com/questions/547996/%D0%9A%D0%B0%D0%BA-%D0%BF%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D1%88%D0%B0%D1%82%D1%8C-%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE-%D0%BF%D0%B5%D1%80%D0%B5%D1%81%D1%82%D0%B0%D0%B2%D0%B8%D1%82%D1%8C-%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D1%8B-%D0%B2-%D0%BC%D0%B0%D1%81%D1%81%D0%B8%D0%B2%D0%B5
-        /// <param name="list"> IList - представляет неуниверсальную коллекцию объектов, к каждому из которых можно получить индивидуальный доступ по индексу.</param>
+        /// <param name="inNames"> IList - представляет неуниверсальную коллекцию объектов, к каждому из которых можно получить индивидуальный доступ по индексу.</param>
         /// </summary>
-        public static void Shuffle<T>( IList<T> list) {  // Переделать
-            for (int n = list.Count-1; n >= 1; n--) {
-                int k = rng.Next(n + 1);
-                T value = list[k];
-                list[k] = list[n];
-                list[n] = value;
+        public static void Shuffle<T>( IList<T> inNames) {  // Переделать
+            for (int indTo = inNames.Count-1; indTo >= 1; indTo--) {
+                int indFrom = randomChange.Next(indTo + 1);
+                T buffValue = inNames[indFrom];
+                inNames[indFrom] = inNames[indTo];
+                inNames[indTo] = buffValue;
             }
         }
     }
